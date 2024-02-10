780ee841d422760be8bbeecaa4c117898abba161
adefuyeabayomi <63407534+adefuyeabayomi@users.noreply.github.com>
2024-02-10 10:30:40 +0100

    Updated

--- src/components/ManualSignUp/ManualSignup.jsx
+++ src/components/ManualSignUp/ManualSignup.jsx
@@ -11,7 +11,7 @@ import { useNavigate } from "react-router-dom";
 function ManualSignup() {
   let navigate = useNavigate();
   let user = useSelector((state)=> state.user)
-  let dispatch = useDispatch();
+  let dispatch = useDispatch()
 
   let [email, setEmail] = useState("");
   let [password, setPassword] = useState("");
--- src/components/VerifyEmail/VerifyEmail.jsx
+++ src/components/VerifyEmail/VerifyEmail.jsx
@@ -4,21 +4,28 @@ import {useState} from "react"
 import { reqSubmitOTP } from "../../utils/requests";
 import {useSelector, useDispatch} from "react-redux"
 import { updateEmail } from "../../storeUtils/userSlice";
+import { Button } from "antd";
 
 function VerifyEmail() {
   let [OTP, setOTP] = useState("");
   let user = useSelector((state)=> state.user)
-  function sendOTP (OTP) {
-    reqSubmitOTP(OTP).then(res=> {
+  let [loading,setLoading]= useState(false)
+  function sendOTP () {
+    let data = {email: user.email, verification_code: OTP}
+    setLoading(true)
+    console.log({data})
+    reqSubmitOTP(data).then(res=> {      
+    setLoading(false)
       alert(JSON.stringify(res))
     }).catch(err=>{
+      setLoading(false)
       alert(JSON.stringify(err))
     })
   }
   return (
-    <div id="verify-email">
+    <div id="verify-email" className="text-center p-3">
       <div className="offset-top-main"></div>
-      <h1>Verify this Number</h1>
+      <h2>Verify this Number</h2>
       <p className="py-4">
         We have sent an OTP to this email. Please input the OTP below
       </p>
@@ -26,9 +33,19 @@ function VerifyEmail() {
       <OTPInput value={OTP} onChange={(e)=>{setOTP(e)}} autoFocus OTPLength={6} otpType="alphanumeric" disabled={false} />
       </div>
       <p className="text-body-bold py-2">Resend OTP <i className="fas fa-repeat-alt"></i></p>
-      <button>Verify Button</button>
+      <div className="pt-3 col-10 col-md-8 center">
+      <Button
+            loading={loading}
+            size="large"
+            className="color-bg-primary color-fg-primary"
+            onClick={sendOTP}
+            shape="round"
+          >
+            Verify Email
+          </Button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default VerifyEmail;
